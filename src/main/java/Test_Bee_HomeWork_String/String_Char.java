9ca79f30c715dcd69ac16f5e3cb8923676cd79fe
dilshad816 <deepadilshad8@gmail.com>
2022-07-25 19:35:33 -0500

    FirstHomeWork

---
+++ src/main/java/Test_Bee_HomeWork_String/String_Char.java
@@ -0,0 +1,31 @@
+package Test_Bee_HomeWork_String;
+
+public class String_Char {
+
+	/*
+	 * 
+	 * Two ways to create string 
+	 * String literal etc : String s = "i like automating software";
+	 * by using new memory operator. etc : String   st = new String("like automating software");
+	 * Using String buffer and String builder
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+		String country ="United States Of America";
+		
+	     System.out.println("United States Of America   "   +country.length()); 
+	     
+	     System.out.println(country.charAt(11));
+	     
+	     System.out.println("I am first t " +country.indexOf("t"));
+	     
+	     System.out.println("I am second t " + country.indexOf("t",country.indexOf("t")+1));
+	     
+	     System.out.println("I am 3rd  t " + country.indexOf("t",country.indexOf("t",country.indexOf("t")+1)));
+	     
+	     System.out.println("I am last t " + country.lastIndexOf("t"));
+	}
+
+}
---
+++ src/main/java/Test_Bee_HomeWork_String/String_Concat.java
@@ -0,0 +1,19 @@
+package Test_Bee_HomeWork_String;
+
+public class String_Concat {
+
+	public static void main(String[] args) {
+	
+		
+		
+		//String concatenation   
+		     
+		     String a1 ="Dilshad";
+		     String a2 ="Deepa";
+		     
+		     a1.concat(a2);// it will add Dilshad + Deepa
+		     System.out.println( a1.concat(a2));
+
+	}
+
+}
