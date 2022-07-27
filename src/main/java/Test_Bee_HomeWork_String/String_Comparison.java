5338bb7361e9c927245aa4cf76cdd61b9f2b6519
dilshad816 <deepadilshad8@gmail.com>
2022-07-26 19:25:45 -0500

    second push

---
+++ src/main/java/Test_Bee_HomeWork_String/String_Comparison.java
@@ -0,0 +1,18 @@
+package Test_Bee_HomeWork_String;
+
+public class String_Comparison {
+
+	public static void main(String[] args) {
+		
+	     String d1="Dilshad Islam Deepa";
+	     String d2=" vzdjklv   vdvndklbn vkdlvbdnbn";
+	     
+	     d1.equals(d2);
+	     
+	     System.out.println(d1.equals(d2));
+	     
+	    System.out.println(d1.equalsIgnoreCase(d2));
+
+	}
+
+}
---
+++ src/main/java/Test_Bee_HomeWork_String/String_Substring.java
@@ -0,0 +1,15 @@
+package Test_Bee_HomeWork_String;
+
+public class String_Substring {
+
+	public static void main(String[] args) {
+		
+		 String st = "I love my country So much"; 
+		
+	     
+	     System.out.println(st.substring(10, 14)); 
+		 
+
+	}
+
+}
